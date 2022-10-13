fc8407bdf3536138f59c1796939e549ce0b93789
Konstantin Biliunas <biliunas13f@gmail.com>
2022-10-14 04:58:48 +0600

    fix code

--- src/index.js
+++ src/index.js
@@ -1,27 +1,19 @@
-// Библиотеки
 import React from "react";
 import ReactDOM from "react-dom/client";
 import store from "./store/store";
 import { Provider } from "react-redux";
-// Контекст
-// HOC
-// UI компоненты
-// Компоненты
+import { REPO_NAME } from "./constants/repo";
+
 import App from "./containers/App";
 import ThemeProvider from "./context/ThemeProvider";
-// Изображения
-// Хуки
-// Роуты
+
 import { BrowserRouter } from "react-router-dom";
-// Утилиты
-// Сервисы
-// Константы
-// Стили
+
 import "./styles/index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<BrowserRouter>
+	<BrowserRouter basename={`/${REPO_NAME}/`}>
 		<React.StrictMode>
 			<Provider store={store}>
 				<ThemeProvider>
